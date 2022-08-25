5b9ebdb84d95a7666e4433ac3b52e3c2ad23b4c3
KillianE2C77 <sta.killian.benigno@e2c77.org>
2022-08-25 16:56:58 +0200

    update code

--- src/app.js
+++ src/app.js
@@ -1,19 +1,9 @@
 import _ from "lodash";
 import "./assets/css/styles.css";
 
-function component() {
-  const element = document.createElement("div");
+const Deposit = document.getElementById("Deposit");
+const ButtonDeposit = document.getElementById("ButtonDeposit");
+const Withdraw = document.getElementById("Withdraw");
+const ButtonWithdraw = document.getElementById("buttonWithdraw");
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("text-3xl");
-
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  return element;
-}
-
-document.body.appendChild(component());
+btnWithdraw.addEventListener;
