27476695390d1efef2a2754ce9d4acf56ea0e314
KillianE2C77 <sta.killian.benigno@e2c77.org>
2022-08-31 16:52:55 +0200

    updateded bank

--- src/app.js
+++ src/app.js
@@ -1,9 +1,48 @@
 import _ from "lodash";
 import "./assets/css/styles.css";
-
-const Deposit = document.getElementById("Deposit");
+const addMoney = document.getElementById("Deposit");
 const ButtonDeposit = document.getElementById("ButtonDeposit");
 const Withdraw = document.getElementById("Withdraw");
 const ButtonWithdraw = document.getElementById("buttonWithdraw");
+const addMoney = [];
+const Withdrawal = [];
+let totalBalance = 25;
 
-btnWithdraw.addEventListener;
+ButtonDeposit.addEventListener("click", () => {
+  if (useraddMoney.value < 0.01 || useraddMoney.value > 10000) {
+    alert("blablabla.");
+  }
+});
+
+let user = {
+  numberAccount: 12548545,
+  iban: "FR76584755",
+  codeBank: 454578,
+  addMoney: 454,
+};
+
+class User {
+  constructor(
+    numberAccount,
+    iban,
+    codeBank,
+    addMoney,
+    withdrawMoney,
+    giveMoney,
+    balanceManagement,
+    authorizedOverdraft,
+    startingMoney
+  ) {
+    this.numberAccount = numberAccount;
+    this.iban = iban;
+    this.codeBank = codeBank;
+    this.addMoney = addMoney;
+    this.withdrawMoney = withdrawMoney;
+    this.giveMoney = giveMoney;
+    this.balanceManagement = balanceManagement;
+    this.authorizedOverdraft = authorizedOverdraft;
+    this.startingMoney = startingMoney;
+  }
+}
+
+let ruben = new User(12548545, "FR76584755", 454578, 25);
