9811f4ea7563504120bd4bcee3c3e202846e2c49
KillianE2C77 <sta.killian.benigno@e2c77.org>
2022-09-05 16:53:25 +0200

    minor update 2

--- src/app.js
+++ src/app.js
@@ -1,15 +1,5 @@
 import "./assets/css/styles.css";
-import { Shirt, Shoes } from "./clothes";
 
-let KillianShirt = new Shirt("blue", "XXL", "Nike", "shirt");
-let KillianShoes = new Shoes("white", "34", "Abibas", "shoes");
-KillianShirt.FindCategory();
-let aze = 0;
-
-let solde = 5;
-solde <= -100 ? console.log("true") : console.log("false");
-
-console.log(KillianShirt, KillianShoes);
 //button
 const ButtonDeposit = document.getElementById("ButtonDeposit");
 const ButtonWithdraw = document.getElementById("buttonWithdraw");
@@ -19,47 +9,22 @@ const ButtonWithdraw = document.getElementById("buttonWithdraw");
 const Deposit = document.getElementById("Deposit");
 const WithdrawMoney = document.getElementById("Withdraw");
 const result = document.getElementById("result");
+const deposits = [];
+const withdrawals = [];
 //Ajout/retrait argent
 
-
-
 //algo
-let amount = Deposit;
-let startingMoney = 25;
-let sold = 0;
+let amount = [];
+let initialSold = 0;
 //algo
-
-// const add = (value) => {
-//   return (sold += value);
-// };
-// console.log(add(5));
-// console.log(sold);
-
-// console.log(add(5));
-// console.log(sold);
-
-// let a = 50;
-// let b = 56;
-// Math.random(a, b);
-
-// function PutMoney() {
-//   //Deposit.startingMoney = Deposit.startingMoney + amount;
-//   Deposit.value;
-
-//   //if (a < b) {
-//   alert("test");
-// }
-
 //button click
-ButtonDeposit.addEventListener("click", () => {
-  console.log(test);
-});
 
-let user = {
-  numberAccount: 12548545,
-  iban: "FR76584755",
-  codeBank: 454578,
-};
+// let user = {
+//   numberAccount: 12548545,
+//   iban: "FR76584755",
+//   codeBank: 454578,
+//   initialSold: 0,
+// };
 
 class User {
   constructor(
@@ -71,7 +36,7 @@ class User {
     giveMoney,
     balanceManagement,
     authorizedOverdraft,
-    startingMoney
+    initialSold
   ) {
     this.numberAccount = numberAccount;
     this.iban = iban;
@@ -81,10 +46,18 @@ class User {
     this.giveMoney = giveMoney;
     this.balanceManagement = balanceManagement;
     this.authorizedOverdraft = authorizedOverdraft;
-    this.startingMoney = startingMoney;
+    this.initialSold = initialSold;
   }
 }
+let ruben = new User(12548545, "FR76584755", 454578, 25, 0, 0, 0, 0, 100);
 
-let ruben = new User(12548545, "FR76584755", 454578, 25);
+//button click
+ButtonDeposit.addEventListener("click", () => {
+  console.log((ruben.initialSold += ruben.Deposit));
+  // console.log(user);
+  // deposits.push(Number(Deposit.value));
+  // initialSold += Number(Deposit.value);
+});
+
+// console.log(ruben.initialSold + 100);
 let safwan = new User(12548545, "FR76584755", 454578, 25);
-
